c612a61fc5e45066a3d0fa2276cb5b4b622d2b65
Pedro Raul <pedroarellano2509@gmail.com>
2026-05-13 21:29:20 -0500

    fix(ui): make business side panel span full height to bottom

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -209,7 +209,7 @@ const Home = () => {
         </div>
 
         <div className={`
-          fixed inset-y-[73px] right-0 w-full md:w-[350px] lg:w-[400px] bg-white border-l border-gray-200 shadow-2xl transform transition-transform duration-300 ease-in-out z-40 overflow-y-auto flex flex-col
+          fixed top-[73px] bottom-0 right-0 w-full md:w-[350px] lg:w-[400px] bg-white border-l border-gray-200 shadow-2xl transform transition-transform duration-300 ease-in-out z-40 overflow-y-auto flex flex-col
           ${selectedBusiness ? 'translate-x-0' : 'translate-x-full'}
         `}>
           {selectedBusiness && (
